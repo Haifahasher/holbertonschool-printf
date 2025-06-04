8c59c581a3c30ed1e5e36269eef33d4bedbd7784
Haifahasher <Haifahasher@gmail.com>
2025-06-04 08:20:49 +0000

    Add print_integer helper to helpers.c

--- helpers.c
+++ helpers.c
@@ -45,3 +45,55 @@ int print_percent(void)
 		return (-1);
 	return (1);
 }
+
+/**
+ * print_integer - Prints a signed integer in base 10.
+ * @n: The integer to print.
+ *
+ * Return: Number of characters printed (including '-'), or
+ *         -1 on write failure.
+ */
+int print_integer(int n)
+{
+	unsigned int abs_val;
+	char buffer[12];
+	int i;
+	int count;
+
+	if (n < 0)
+	{
+		if (write(1, "-", 1) == -1)
+			return (-1);
+		count = 1;
+		abs_val = (unsigned int)(-1 * (long)n);
+	}
+	else
+	{
+		count = 0;
+		abs_val = (unsigned int)n;
+	}
+
+	if (abs_val == 0)
+	{
+		if (write(1, "0", 1) == -1)
+			return (-1);
+		return (count + 1);
+	}
+
+	i = 0;
+	while (abs_val > 0)
+	{
+		buffer[i] = (char)('0' + (abs_val % 10));
+		abs_val /= 10;
+		i++;
+	}
+
+	while (i-- > 0)
+	{
+		if (write(1, &buffer[i], 1) == -1)
+			return (-1);
+		count++;
+	}
+
+	return (count);
+}
